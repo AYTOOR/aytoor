f528c2e40081731dc39714fadf9aec47c1991660
AYTOOR <32774745+AYTOOR@users.noreply.github.com>
2017-10-14 15:47:31 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,5 +1,5 @@
--- #Beyond Reborn Robot
--- #@BeyondTeam
+-- #aytor Reborn Robot
+-- #@mohammadbots
 
 tdcli = dofile('./tg/tdcli.lua')
 serpent = (loadfile "./libs/serpent.lua")()
@@ -90,7 +90,7 @@ function create_config( )
 		"fun",
 
 	},
-    sudo_users = {111334847, 157059515, sudo_id},
+    sudo_users = {431714063, 431714063, sudo_id},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
